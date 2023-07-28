1a63eaab622db0d8da57ec09ee1099c9245b1903
Precious Affiah <74688829+preciousaffiah@users.noreply.github.com>
2023-07-28 12:23:19 +0100

    Update APIroutes.js

--- src/public/utils/APIroutes.js
+++ src/public/utils/APIroutes.js
@@ -1,5 +1,5 @@
-// export const host = "https://e-commerce-8pag.onrender.com";
-export const host = "http://localhost:3001";
+export const host = "https://e-commerce-8pag.onrender.com";
+//export const host = "http://localhost:3001";
 export const getallItems = `${host}/items`;
 export const getItem = `${host}/items/`;
 export const getcart = `${host}/users/mycart`;
